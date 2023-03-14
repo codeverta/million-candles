e10fdff845d76d6a3b06efb418a05d3cd08302dd
rabihutomo <rabihutomo11@gmail.com>
2023-03-14 23:00:20 +0700

    user

---
+++ backend/app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use LaravelJsonApi\Laravel\Http\Controllers\Actions;
+
+class UserController extends Controller
+{
+
+    use Actions\FetchMany;
+    use Actions\FetchOne;
+    use Actions\Store;
+    use Actions\Update;
+    use Actions\Destroy;
+    use Actions\FetchRelated;
+    use Actions\FetchRelationship;
+    use Actions\UpdateRelationship;
+    use Actions\AttachRelationship;
+    use Actions\DetachRelationship;
+
+    public function creating(User $user)
+    {
+        
+    }
+}
