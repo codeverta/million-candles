aa7660650d95f8e80b55dd3114d18f5c29d8a831
rabihutomo <rabihutomo11@gmail.com>
2023-04-19 17:31:44 +0700

    fix: notif pt 2

--- backend/app/Notifications/OrdersNotification.php
+++ backend/app/Notifications/OrdersNotification.php
@@ -78,9 +78,7 @@ class OrdersNotification extends Notification
     public function toArray($notifiable)
     {
         if(!isset($this->order->code)) {
-            return [
-                'data' => $this->order,
-            ];
+            return $this->order;
         }
         return [
             'code' => $this->order->code,
