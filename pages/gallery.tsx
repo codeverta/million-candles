002cd4b4151956f4687c6741fbc862ed73c034f9
rabihutomo <rabihutomo11@gmail.com>
2024-08-23 09:52:24 +0700

    title

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -56,11 +56,10 @@ const PhotoGallery = () => {
       <div className="container mx-auto py-12">
         <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Spoonie Seneng
+            Gallery
           </h2>
           <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
-            Spoonie Seneng adalah Kegiatan Rutin Mingguan sebagai Penghibur
-            Pasien.
+            Kumpulan Foto Produk Kami
           </p>
         </div>
         <Gallery>
