b15cd478b15e9559fd5ea5b4f602664a6707307a
Rabih Utomo <rabihutomo11@gmail.com>
2025-05-14 08:46:10 +0000

    cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://souvenirlilin.id'],
+    'allowed_origins' => ['https://www.souvenirlilin.id'],
 
     'allowed_origins_patterns' => [],
 
