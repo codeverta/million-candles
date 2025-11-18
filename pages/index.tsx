c602f586f08cef4df4c306db113586ff4379d94d
rabihutomo <rabihutomo11@gmail.com>
2025-11-18 14:23:21 +0700

    fix: bug default url

--- pages/index.tsx
+++ pages/index.tsx
@@ -600,7 +600,7 @@ function Home(props: any) {
                             </Link>
                           ) : (
                             <Link
-                              href={`/en/posts/${post.id}`}
+                              href={`${process.env.SITE_URL}/en/posts/${post.id}`}
                               className="cursor-pointer hover:underline"
                             >
                               {post.title}
