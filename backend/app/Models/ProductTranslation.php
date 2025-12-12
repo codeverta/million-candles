c0016d0d779282b35f0452455b2548250fd49ab3
rabihutomo <rabihutomo11@gmail.com>
2025-12-12 15:29:13 +0700

    fix: product translation

--- backend/app/Models/ProductTranslation.php
+++ backend/app/Models/ProductTranslation.php
@@ -28,4 +28,28 @@ class ProductTranslation extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    protected static function booted(): void
+    {
+        // Panggil setelah ProductTranslation disimpan (dibuat/diperbarui)
+        static::saved(function (ProductTranslation $translation) {
+            // ATAU Anda bisa memilih locale mana pun untuk menjadi dasar slug.
+            $defaultLocale = config('app.locale');
+
+            if ($translation->locale === $defaultLocale) {
+                $product = $translation->product;
+                
+                // Pastikan nama ada
+                if ($translation->name && $product) {
+                    $slug = Str::slug($translation->name);
+                    
+                    // Hanya simpan jika slug berubah untuk menghindari loop
+                    if ($product->slug !== $slug) {
+                        $product->slug = $slug;
+                        $product->saveQuietly(); // Gunakan saveQuietly() untuk menghindari memicu event lagi
+                    }
+                }
+            }
+        });
+    }
 }
